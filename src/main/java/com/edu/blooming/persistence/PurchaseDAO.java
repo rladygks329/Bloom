2fea1acc0bc71bdd3ac69c147c13abc5f32dc0f5
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2023-10-24 12:36:16 +0900

    update: purchase
    구매 목록 확인
    물건 구매 유무 확인 기능

--- src/main/java/com/edu/blooming/persistence/PurchaseDAO.java
+++ src/main/java/com/edu/blooming/persistence/PurchaseDAO.java
@@ -1,8 +1,14 @@
 package com.edu.blooming.persistence;
 
+import java.util.List;
+import com.edu.blooming.domain.LectureVO;
+
 public interface PurchaseDAO {
   int insert(int memberId, int lectureId, int price);
 
   int delete(int memberId, int lectureId);
 
+  List<LectureVO> select(int memberId);
+
+  boolean selectIsMemberBuyLecture(int memberId, int lectureId);
 }
--- src/main/java/com/edu/blooming/service/PurchaseService.java
+++ src/main/java/com/edu/blooming/service/PurchaseService.java
@@ -1,6 +1,7 @@
 package com.edu.blooming.service;
 
 import java.util.List;
+import com.edu.blooming.domain.LectureVO;
 
 public interface PurchaseService {
 
@@ -10,4 +11,7 @@ public interface PurchaseService {
 
   int refund(int memberId, int lectureId);
 
+  List<LectureVO> getPurchaseList(int memberId);
+
+  boolean checkPurchase(int memberId, int lectureId);
 }
