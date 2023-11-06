7095415ab8c08903893e0a2fa964f5cb7e5c85ab
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2023-11-06 11:45:40 +0900

    feat: alreadyExist exception
    도매인 친화적인 "이미 존재합니다" 에러를 만들었습니다
    runtime에러를 상속하여 transction을 취소할 수 있습니다.
    controller adice에서 한번에 처리할 수 있도록 만들었습니다.

---
+++ src/main/java/com/edu/blooming/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.edu.blooming.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.edu.blooming.exception.AlreadyExistException;
+
+@ControllerAdvice
+public class ExceptionController {
+  private Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+  @ExceptionHandler(AlreadyExistException.class)
+  public ResponseEntity<String> HandleAlreadyExistException(final RuntimeException ex) {
+    logger.info("HandleAlreadyExistException() 실행");
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+  }
+}
