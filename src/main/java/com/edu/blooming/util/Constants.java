3caf73656faeee771a35fd3957a33aa8f505ddbe
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2023-11-22 11:43:13 +0900

    update: 상수 변경

--- src/main/java/com/edu/blooming/util/Constants.java
+++ src/main/java/com/edu/blooming/util/Constants.java
@@ -2,4 +2,6 @@ package com.edu.blooming.util;
 
 public class Constants {
   public static final int MAX_REFUND_LIMIT = 14;
+  public static final int VID_PROC_LEVEL_NOT_READY = 0;
+  public static final int VID_PROC_LEVEL_COMPLETE = 1;
 }
