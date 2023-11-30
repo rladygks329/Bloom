7067fdaef25ba94978ece7d4f12ee651fcbab771
cho100323 <146702294+cho100323@users.noreply.github.com>
2023-11-30 18:21:35 +0900

    feat: mailHandler 작업

--- src/main/java/com/edu/blooming/util/MailHandler.java
+++ src/main/java/com/edu/blooming/util/MailHandler.java
@@ -1,5 +1,15 @@
 package com.edu.blooming.util;
 
-public class MailHandler {
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+public class MailHandler {
+  private JavaMailSender mailSender;
+  private MimeMessage message;
+  private MimeMessageHelper messageHelper;
+  
+  
+  
+  Mimi
 }
